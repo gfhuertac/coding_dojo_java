c5a1ab7fdff732c73edbc53a1b3615db12c7c4d1
Gonzalo Huerta-Canepa <gonzalo@huerta.cl>
2020-01-28 16:40:19 -0300

    Java: Calculator II

--- calculator/src/com/codingdojo/calculator/Calculator.java
+++ calculator/src/com/codingdojo/calculator/Calculator.java
@@ -18,30 +18,41 @@ public class Calculator {
     private double lastResult;
     private List<Double> results;
 
+    private List<String> operations;
+    private List<Double> values;
+
     public Calculator() {
         results = new ArrayList<>();
+        operations = new ArrayList<>();
+        values = new ArrayList<>();
     }
 
+    @Deprecated
     public double getOperandOne() {
         return operandOne;
     }
 
+    @Deprecated
     public void setOperandOne(double operandOne) {
         this.operandOne = operandOne;
     }
 
+    @Deprecated
     public double getOperandTwo() {
         return operandTwo;
     }
 
+    @Deprecated
     public void setOperandTwo(double operandTwo) {
         this.operandTwo = operandTwo;
     }
 
+    @Deprecated
     public OPERATION getOperation() {
         return operation;
     }
 
+    @Deprecated
     public void setOperation(OPERATION operation) {
         this.operation = operation;
     }
@@ -66,24 +77,80 @@ public class Calculator {
         this.results.add(result);
     }
 
+    @Deprecated
     public void performOperation() {
         if (operation == null)
             throw new IllegalArgumentException("operation cannot be null");
-        switch(operation) {
-            case ADDITION:
-                lastResult = operandOne + operandTwo;
-                break;
-            case SUBTRACTION:
-                lastResult = operandOne - operandTwo;
-                break;
-            case MULTIPLICATION:
-                lastResult = operandOne * operandTwo;
-                break;
-            case DIVISION:
-                lastResult = operandOne / operandTwo;
-                break;
+        switch (operation) {
+        case ADDITION:
+            lastResult = operandOne + operandTwo;
+            break;
+        case SUBTRACTION:
+            lastResult = operandOne - operandTwo;
+            break;
+        case MULTIPLICATION:
+            lastResult = operandOne * operandTwo;
+            break;
+        case DIVISION:
+            lastResult = operandOne / operandTwo;
+            break;
         }
         addResult(lastResult);
+    }
+
+    private int getHighestPriorityIndex() {
+        int rv = operations.indexOf("*");
+        if (rv == -1) {
+            rv = operations.indexOf("/");
+        }
+        if (rv == -1) {
+            rv = operations.indexOf("+");
+        }
+        if (rv == -1) {
+            rv = operations.indexOf("-");
+        }
+        return rv;
+    }
+
+    public void performOperation(double item) {
+        values.add(item);
+    }
+
+    public void performOperation(String item) {
+        if ("=".equals(item)) {
+            while (operations.size() != 0) {
+                int idx = getHighestPriorityIndex();
+                if (idx == -1)
+                    throw new IllegalArgumentException("No valid operation found!");
+                String operator = operations.get(idx);
+                double operandOne = values.get(idx);
+                double operandTwo = values.get(idx + 1);
+                double result = 0.0;
+                switch (operator) {
+                case "*":
+                    result = operandOne * operandTwo;
+                    break;
+                case "/":
+                    result = operandOne / operandTwo;
+                    break;
+                case "+":
+                    result = operandOne + operandTwo;
+                    break;
+                case "-":
+                    result = operandOne - operandTwo;
+                    break;
+                }
+                values.remove(idx+1);
+                values.set(idx, result);
+                operations.remove(idx);
+            }
+            lastResult = values.get(0);
+            results.add(lastResult);
+            values.clear();
+            operations.clear();
+        } else {
+            operations.add(item);
+        }
     }
 
 }
--- calculator/src/com/codingdojo/calculator/CalculatorTest.java
+++ calculator/src/com/codingdojo/calculator/CalculatorTest.java
@@ -11,6 +11,21 @@ public class CalculatorTest {
         calculator.setOperation(Calculator.OPERATION.ADDITION);
         calculator.setOperandTwo(5.2);
         calculator.performOperation();
+
+        calculator.performOperation(10.5);
+        calculator.performOperation("+");
+        calculator.performOperation(5.2);
+        calculator.performOperation("*");
+        calculator.performOperation(10);
+        calculator.performOperation("=");
+
+        calculator.performOperation(10.5);
+        calculator.performOperation("*");
+        calculator.performOperation(5.2);
+        calculator.performOperation("/");
+        calculator.performOperation(10);
+        calculator.performOperation("=");
+        
         System.out.println(calculator.getLastResult());
         calculator.getResults().stream().forEach((i)->System.out.println(i));
     }
